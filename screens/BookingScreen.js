8aa0d5211d17bd22f6845e0b6ef984b84f238a89
hanva100 <nicolas.tran@supinternet.fr>
2019-11-13 23:20:17 +0100

    add some styles

--- screens/BookingScreen.js
+++ screens/BookingScreen.js
@@ -1,45 +1,54 @@
 import React, {Component} from 'react';
-import {View,
+import {
+    View,
     StyleSheet,
     FlatList,
     Text,
     Dimensions,
-    Button} from 'react-native';
+    TouchableOpacity
+} from 'react-native';
+import HomeScreen from "./HomeScreen";
+import Image from "react-native-web/dist/exports/Image";
 
 export default class BookingScreen extends Component {
     constructor(props) {
         super(props);
-        this.state= {
-          elements: [
-                {id:0,name:'yo',price:100000,country:"france",city:"paris",booking_left:50},
-                {id:0,name:'yo',price:100000,country:"france",city:"paris",booking_left:50},
-                {id:0,name:'yo',price:100000,country:"france",city:"paris",booking_left:50},
+        this.state = {
+            elements: [
+                {id: 0, name: 'yo', price: 100000, country: "france", city: "paris", booking_left: 50},
+                {id: 0, name: 'yo', price: 100000, country: "france", city: "paris", booking_left: 50},
+                {id: 0, name: 'yo', price: 100000, country: "france", city: "paris", booking_left: 50},
             ],
-      }
-      }
+        }
+    }
 
-    renderItem({ item, index }) { 
-        return(
-         <View style={styles.item} >
-             <Text>{item.name}</Text>
-             <Text>{item.country}</Text>
-             <Text>{item.city}</Text>
-             <Text>{item.price}€</Text>
-             <Text>Il reste {item.booking_left} places</Text>
-             <Button title="Réserver"></Button>
-         </View>
+    renderItem({item, index}) {
+        return (
+            <View style={styles.item}>
+                <View>
+                    <Text style={styles.white}>{item.name}</Text>
+                    <Text style={styles.white}>{item.country}</Text>
+                    <Text style={styles.white}>{item.city}</Text>
+                    <Text style={styles.white}>{item.price}€</Text>
+                    <Text style={styles.white}>Il reste {item.booking_left} places</Text>
+                </View>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Réserver</Text>
+                </TouchableOpacity>
+            </View>
         )
-      }
+    }
+
     render() {
         return (
             <View style={[styles.container]}>
-                 <FlatList
-                renderItem={this.renderItem}
-                data={this.state.elements}
-                style={styles.listItem} />
+                <View style={styles.bar}/>
+                <Text style={styles.title}>{'réservation'.toUpperCase()}</Text>
+                <FlatList
+                    renderItem={this.renderItem}
+                    data={this.state.elements}
+                    style={styles.listItem}/>
             </View>
-            
-
         );
     }
 }
@@ -47,17 +56,51 @@ let Window = Dimensions.get('window');
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-      },
-      listItem: {
-        flex: 1,
-      },
-    item: {
-        marginTop: 50,
-        left: 15,
-        width: Window.width - 30,
-        height: 200,
-        borderWidth: 1,
-        borderColor: 'red'
+        backgroundColor: '#363F46',
     },
+    bar: {
+        width: 2,
+        height: 25,
+        top: 40,
+        left: 30,
+        position: "absolute",
+        backgroundColor: "#41FFE1",
+    },
+    title: {
+        top: 40,
+        fontFamily: 'title-font',
+        fontSize: 25,
+        color: "#ffffff",
+        left: 50
+    },
+    listItem: {
+        flex: 1,
+    },
+    item: {
+        backgroundColor: "#48545C",
+        fontFamily: 'text-font',
+        marginTop: 50,
+        left: 30,
+        width: Window.width - 60,
+        height: 200,
+        borderRadius: 10,
+        borderColor: 'red'
+    }, button: {
+        width: 200,
+        textAlign: "center",
+        backgroundColor: "#41FFE1",
+        borderRadius: 10,
+        color: "#363F46"
+    },
+    buttonText: {
+        textAlign: "center",
+        fontFamily: 'text-font',
+        fontSize: 20
+    },
+    white: {
+        color: "#ffffff"
+    }
 })
+BookingScreen.navigationOptions = {
+    header: null,
+};
