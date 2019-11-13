64ff9f4a47cb322f81cfe5326a6639a9d4732582
Pakopac <lilian.pacaud@supinternet>
2019-11-13 22:58:18 +0100

    Get and Post bookings

--- screens/BookingScreen.js
+++ screens/BookingScreen.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import {View,
     StyleSheet,
     FlatList,
@@ -6,27 +7,87 @@ import {View,
     Dimensions,
     Button} from 'react-native';
 
+import getEnvVars from "../config/env";
+
+const {apiUrl} = getEnvVars();
+
 export default class BookingScreen extends Component {
     constructor(props) {
         super(props);
         this.state= {
-          elements: [
-                {id:0,name:'yo',price:100000,country:"france",city:"paris",booking_left:50},
-                {id:0,name:'yo',price:100000,country:"france",city:"paris",booking_left:50},
-                {id:0,name:'yo',price:100000,country:"france",city:"paris",booking_left:50},
-            ],
+          elements: [],
+        }
+        this.getData= this.getData.bind(this);
+        this.test = this.test.bind(this);
       }
-      }
+
+    sendBooking(){
+        console.log(apiUrl)
+        axios.post(`${apiUrl}/reserveBooking`, {
+                "id": 1,
+                "email": "a@a.com",
+            }
+        ).then(function (response) {
+            console.log(response.data);
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    componentDidMount(){
+        this.getData().then((response) => {
+            console.log(response)
+            this.setState({
+                elements: response
+            })
+            console.log(this.state.elements)
+          })
+    }
+
+    async getData() {
+        return new Promise((resolve, reject) => {
+            axios.get(`${apiUrl}/getBookings`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then(function (response) {
+                    console.log(response.data);
+                    resolve(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        })
+    }
+
+    test(item){
+        return console.log(item)
+    }
 
     renderItem({ item, index }) { 
         return(
-         <View style={styles.item} >
+         <View style={styles.item}>
              <Text>{item.name}</Text>
              <Text>{item.country}</Text>
              <Text>{item.city}</Text>
              <Text>{item.price}€</Text>
-             <Text>Il reste {item.booking_left} places</Text>
-             <Button title="Réserver"></Button>
+             <Text>Il reste {item.bookings_left} places</Text>
+             <Button title="Réserver" onPress={() => {
+                 axios.post(`${apiUrl}/reserveBooking`, {
+                    "id": item.id,
+                    "email": "a@a.com",
+                }
+                ).then(function (response) {
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+             }}></Button>
          </View>
         )
       }
